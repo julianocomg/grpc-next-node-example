764e16d2824b014c4922605aeb94715373f249b7
Juliano Duarte <julianocomg@gmail.com>
2020-11-26 11:41:51 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,15 +11,11 @@ const TodoProto = loadPackageDefinition(
   loadProtoFile('../protos/todo.proto', { keepCase: true })
 );
 
-function main() {
-  var server = new Server();
+var server = new Server();
 
-  server.addService(TodoProto.todo.TodoList.service, TodoService.default);
+server.addService(TodoProto.todo.TodoList.service, TodoService.default);
 
-  server.bindAsync('0.0.0.0:9090', ServerCredentials.createInsecure(), () => {
-    server.start();
-    console.log('gRPC TodoList server running at 127.0.0.1:9090');
-  });
-}
-
-main();
+server.bindAsync('0.0.0.0:9090', ServerCredentials.createInsecure(), () => {
+  server.start();
+  console.log('gRPC TodoList server running at 127.0.0.1:9090');
+});
